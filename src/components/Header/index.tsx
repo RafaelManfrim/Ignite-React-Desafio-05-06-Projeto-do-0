ca6adfe9b5d5cf10167cb349f76c826fabb2e8ed
RafaelManfrim <rafaelmanfrim2004@gmail.com>
2022-01-13 17:57:33 -0300

    Header test 100%

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,12 @@
-export default function Header() {
-  // TODO
+import Link from 'next/link';
+import styles from './header.module.scss';
+
+export default function Header(): JSX.Element {
+  return (
+    <header className={styles.logoArea}>
+      <Link href="/" passHref>
+        <img src="/Logo.svg" alt="logo" />
+      </Link>
+    </header>
+  );
 }
